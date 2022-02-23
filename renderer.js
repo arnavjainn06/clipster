418890dd5e75a7fc5fd83e951a59e90106282dd2
Arnav Jain <jainarnav@outlook.com>
2022-02-23 19:50:04 +0530

    final pr for the day

--- renderer.js
+++ renderer.js
@@ -1,10 +1,12 @@
 const { createApp } = require("vue");
-const { clipboard } = require("electron");
+const { clipboard, ipcMain } = require("electron");
 
 createApp({
     data() {
         return {
             items: [],
+            reversedItems: [],
+            savedItems: [],
         };
     },
     mounted() {
@@ -17,9 +19,31 @@ createApp({
             if (this.items[this.items.length - 1] !== text) {
                 this.items.push(text);
             }
+            this.reversedItems = this.items.slice().reverse();
         },
         copyToClipboard(item) {
             clipboard.writeText(item);
+            this.$refs.notification.style.display = "flex";
+            const index = this.items.indexOf(item);
+            this.items.splice(index, 1);
+
+            setTimeout(() => {
+                this.$refs.notification.style.animation = "opacifyOut 0.4s";
+                setTimeout(() => {
+                    this.$refs.notification.style.display = "none";
+                    this.$refs.notification.style.animation = "opacify 0.4s";
+                }, 400);
+            }, 2000);
+        },
+        copyForTab($event) {
+            if ($event.code === "Enter") {
+                const mainText = $event.path[0].firstChild.textContent;
+                this.copyToClipboard(mainText);
+            }
+        },
+        saveItem(text) {
+            let existing = JSON.parse(localStorage.getItem("saved"));
+            existing.push(text);
         },
     },
 }).mount("#app");
